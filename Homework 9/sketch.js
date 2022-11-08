0308e10ee1f791a42df6fe3b3c4c3d68767afbd4
HarrisonBAR <112789694+HarrisonBAR@users.noreply.github.com>
2022-11-07 22:56:51 -0700

    Homework 10

---
+++ Homework 9/sketch.js
@@ -0,0 +1,24 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw(){
+  circle(200,80,50);
+  strokeWeight(3);
+  point(190,75);
+  point(210,75);
+  line(195,85,200,80);
+  line(195,85,200,85);
+  line(193,92,207,92);
+  line (200,105,200,240);
+  line(200,135,150,165);
+  line(200,135,250,165);
+  line(200,240,180,320);
+  line(200,240,220,320);
+  rect(140,320,120,50)
+  triangle(145,360,165,330,180,360);
+  triangle(220,360,240,330,255,360);
+  text('A Simple Man', 10,30);
+  text('Harrison Bartlett',10,395);
+}
+
